9297a1e4ca28f596a39f69c5b8344052fe21adb4
bishengjie <255077145@qq.com>
2023-04-21 15:09:57 +0800

    feat: 创建管理器容器-08

---
+++ Assets/Resources/ManagerVars.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// [CreateAssetMenu(menuName = "CreatManagerVarsContainer")]
+public class ManagerVars : ScriptableObject
+{
+    public static ManagerVars GetManagerVars()
+    {
+        return Resources.Load<ManagerVars>("ManagerVarsContainer");
+    }
+    public List<Sprite> bgThemeSpriteList = new();
+}
--- Assets/Scripts/UI/BgTheme.cs
+++ Assets/Scripts/UI/BgTheme.cs
@@ -2,15 +2,21 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
+
 //             主题 
 public class BgTheme : MonoBehaviour
 {
     private SpriteRenderer _spriteRenderer;
-    public Sprite[] sprites;
+    private ManagerVars _vars;// 变动
+
 
     private void Awake()
     {
+        _vars = ManagerVars.GetManagerVars();
         _spriteRenderer = GetComponent<SpriteRenderer>();
-        
+        int ranValue = Random.Range(0, _vars.bgThemeSpriteList.Count);
+        _spriteRenderer.sprite = _vars.bgThemeSpriteList[ranValue];
+
     }
 }
